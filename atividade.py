1e56dfb927376dcf3613c9680d240e8673f1b181
heloisamariasl <heloisamsleite@gmail.com>
2025-03-18 14:10:31 -0300

    atividade.py

---
+++ atividade.py
@@ -0,0 +1,7 @@
+nome = "Helô"
+idade = "16"
+poder = "gravitar"
+local =  "caverna"
+print(f"A  lutadora {nome}, de idade {idade}, com o grande poder de {poder}, estava sempre preparada para quem viesse.BOWWWWWBOW PEIPEIPOWPOW!!! Pode vir poww!!")
+
+
