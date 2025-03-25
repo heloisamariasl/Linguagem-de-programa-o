581d618ec2c857874f03c347900dcce4896bb3fb
heloisamariasl <heloisamsleite@gmail.com>
2025-03-25 13:21:36 -0300

    questao1.py

---
+++ ficha1/questao1.py
@@ -0,0 +1,19 @@
+#1
+#Média simples
+N1 = float(input("Digite sua N1:"))
+N2 = float(input("Digite sua N2:"))
+N3 = float(input("Digite sua N3:"))
+media = (N1 + N2 + N3) / 3
+print("Média:", media)
+#Média ponderada (2,2,3)
+N1 = float(input("Digite sua N1:"))
+N2 = float(input("Digite sua N2:"))
+N3 = float(input("Digite sua N3:"))
+media = (N1*2 + N2*2 + N3*3) / 7
+print("Média:", media)
+#Média ponderada(1,2,2)
+N1 = float(input("Digite sua N1:"))
+N2 = float(input("Digite sua N2:"))
+N3 = float(input("Digite sua N3:"))
+media = (N1 + N2*2 + N3*2) / 5
+print("Média:", media)
