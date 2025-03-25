03e450a2b5e270d800486b28c8179f95c6618cbf
heloisamariasl <heloisamsleite@gmail.com>
2025-03-25 13:41:41 -0300

    questao9.py

---
+++ ficha1/questao9.py
@@ -0,0 +1,10 @@
+#9
+numero1 = float(input("Digite o um número:"))
+numero2 = float(input("Digite o outro número:"))
+numero3 = float(input("Digite mais um número:"))
+
+maior = max(numero1, numero2, numero3)
+menor = min(numero1, numero2, numero3)
+
+print(f"O maior número é: {maior}")
+print(f"O menor número é: {menor}")
