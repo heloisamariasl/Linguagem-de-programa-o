43300d115a58214c71ada92ddc329d779c908a60
heloisamariasl <heloisamsleite@gmail.com>
2025-04-04 20:35:01 -0300

    navegação

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// App.js
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
+function HomeScreen({ navigation }) {
+return (
+<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+<Text>Bem-vindo ao Meu App de navegação!</Text>
+<Button title="Próxima tela :)" onPress={() => navigation.navigate('Perfil')} />
+</View>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function PerfilScreen() {
+return (
+<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+<Text>Segunda telaa :|</Text>
+</View>
+);
+}
+export default function App() {
+return (
+<NavigationContainer>
+<Stack.Navigator initialRouteName="Home">
+<Stack.Screen name="Home" component={HomeScreen} />
+<Stack.Screen name="Perfil" component={PerfilScreen} />
+</Stack.Navigator>
+</NavigationContainer>
+);
+}
